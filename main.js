db1f2a5ee7cedc545a87e909fce5c4e475be795b
Kerzyte <1848038+kerzyte@users.noreply.github.com>
2021-12-29 07:15:25 -0600

    Version 0.1.2 Release
    Updated for Cookie Clicker 2.044 beta
    
    Cursor upgrade efficiency corrected and extended
    
    Building efficiency now considers cursor upgrades

--- main.js
+++ main.js
@@ -22,8 +22,67 @@ Game.registerMod("cookie taster",{
 			for (i=0; i<Game.ObjectsById.length; i++) {
 				document.getElementById("productName"+i).style.removeProperty("color");
 				var cost = Game.ObjectsById[i].getSumPrice(Game.buyBulk);
-				buildingEff[i] = cost / (Game.ObjectsById[i].storedCps * Game.globalCpsMult * Math.max(Game.buyBulk, 1));
+				//missing synergies
+				if (i == 0) {
+					buildingEff[i] = cost / (Game.ObjectsById[i].storedCps * Game.globalCpsMult * Math.max(Game.buyBulk, 1));
+				} else {
+					var d_cps = Game.ObjectsById[i].storedCps + calcCursorCPS(Game.ObjectsById[0]);
+					buildingEff[i] = cost / (d_cps * Game.globalCpsMult * Math.max(Game.buyBulk, 1));
+				}
 			}
+		}
+
+		function calcCursorCPS(me, bonus) {
+			// Orteil pls add a better way to do this...
+			var add = bonus == .1 ? bonus : 0;
+			// Excerpt from main.js
+			if (Game.Has('Thousand fingers')) add+=		0.1;
+			if (Game.Has('Million fingers')) add*=		5;
+			if (Game.Has('Billion fingers')) add*=		10;
+			if (Game.Has('Trillion fingers')) add*=		20;
+			if (Game.Has('Quadrillion fingers')) add*=	20;
+			if (Game.Has('Quintillion fingers')) add*=	20;
+			if (Game.Has('Sextillion fingers')) add*=	20;
+			if (Game.Has('Septillion fingers')) add*=	20;
+			if (Game.Has('Octillion fingers')) add*=	20;
+			if (Game.Has('Nonillion fingers')) add*=	20;
+			if (Game.Has('Decillion fingers')) add*=	20;
+			var mult=1;
+			var num=0;
+			for (var i in Game.Objects) {if (Game.Objects[i].name!='Cursor') num+=Game.Objects[i].amount;}
+			//add=add*num;
+			mult*=Game.GetTieredCpsMult(me);
+			mult*=Game.magicCpS('Cursor');
+			mult*=Game.eff('cursorCps');
+			// End Excerpt
+			var power = Game.Has('Reinforced index finger') + Game.Has('Carpal tunnel prevention cream') + Game.Has('Ambidextrous') + (bonus == 1 ? bonus : 0);
+			if (bonus) {
+				var storedCps = Game.ComputeCps(.1, power, add * num * (bonus >= 5 ? bonus : 1)) * mult;
+			} else {
+				var storedCps = add * mult;
+			}
+			// Excerpt from main.js
+			var buildMult=1;
+			if (Game.hasGod)
+			{
+				var godLvl=Game.hasGod('decadence');
+				if (godLvl==1) buildMult*=0.93;
+				else if (godLvl==2) buildMult*=0.95;
+				else if (godLvl==3) buildMult*=0.98;
+				
+				var godLvl=Game.hasGod('industry');
+				if (godLvl==1) buildMult*=1.1;
+				else if (godLvl==2) buildMult*=1.06;
+				else if (godLvl==3) buildMult*=1.03;
+				
+				var godLvl=Game.hasGod('labor');
+				if (godLvl==1) buildMult*=0.97;
+				else if (godLvl==2) buildMult*=0.98;
+				else if (godLvl==3) buildMult*=0.99;
+			}
+			// End Excerpt
+			if (Game.ascensionMode!=1) storedCps*=(1+me.level*0.01)*buildMult;
+			return storedCps * me.amount;
 		}
 
 		function calcUpgradeCPS() {
@@ -34,27 +93,30 @@ Game.registerMod("cookie taster",{
 					upgradeEff[i] = Infinity;
 					continue;
 				}
+				var cookiesPsEarthly = Game.cookiesPs / (1 + Game.prestige * Game.GetHeavenlyMultiplier() / 100);
 				var cost = Game.UpgradesInStore[i].getPrice();
-				var denom = 4;
 				switch (Game.UpgradesInStore[i].id) {
 					//cursor
+					//missing click boost
 					case 0: case 1: case 2:
-						upgradeEff[i] = cost / (Game.globalCpsMult * Game.ObjectsById[0].storedTotalCps);
+						upgradeEff[i] = cost / ((calcCursorCPS(Game.ObjectsById[0], 01) - Game.ObjectsById[0].storedTotalCps) * Game.globalCpsMult);
+						break;
+					case 3:
+						upgradeEff[i] = cost / ((calcCursorCPS(Game.ObjectsById[0], .1) - Game.ObjectsById[0].storedTotalCps) * Game.globalCpsMult);
+						break;
+					case 4:
+						upgradeEff[i] = cost / ((calcCursorCPS(Game.ObjectsById[0], 05) - Game.ObjectsById[0].storedTotalCps) * Game.globalCpsMult);
+						break;
+					case 5:
+						upgradeEff[i] = cost / ((calcCursorCPS(Game.ObjectsById[0], 10) - Game.ObjectsById[0].storedTotalCps) * Game.globalCpsMult);
+						break;
+					case 6: case 43: case 82: case 109: case 188: case 189: case 660: case 764:
+						upgradeEff[i] = cost / ((calcCursorCPS(Game.ObjectsById[0], 20) - Game.ObjectsById[0].storedTotalCps) * Game.globalCpsMult);
 						break;
 					/*
-					3
-					4
-					5
-					6
-					43
-					82
-					109
-					188
-					189
-					660
 					//mouse
 					case 75: case 76: case 77: case 78: case 119: case 190: case 191:
-					case 366: case 367: case 427: case 460: case 461: case 661:
+					case 366: case 367: case 427: case 460: case 461: case 661: case 765:
 						//1% click stuff here
 						break;
 					//golden
@@ -64,10 +126,10 @@ Game.registerMod("cookie taster",{
 					*/
 					//research
 					case 64:
-						upgradeEff[i] = cost / (Game.globalCpsMult * Game.ObjectsById[1].storedTotalCps * 3);
+						upgradeEff[i] = cost / (Game.ObjectsById[1].storedTotalCps * Game.globalCpsMult * 3);
 						break;
 					case 67: case 162:
-						upgradeEff[i] = cost / (Game.globalCpsMult * Game.ObjectsById[1].storedTotalCps);
+						upgradeEff[i] = cost / (Game.ObjectsById[1].storedTotalCps * Game.globalCpsMult);
 						break;
 					/*
 					69
@@ -85,8 +147,17 @@ Game.registerMod("cookie taster",{
 					642
 					*/
 					//kitten
+					case 641:
+						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .050);
+						break;
 					case 31:
 						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .100);
+						break;
+					case 766:
+						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .110);
+						break;
+					case 613:
+						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .115);
 						break;
 					case 32: case 494:
 						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .125);
@@ -100,15 +171,9 @@ Game.registerMod("cookie taster",{
 					case 187: case 320: case 321: case 322: case 425:
 						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .200);
 						break;
-					case 613:
-						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .115);
-						break;
-					case 641:
-						upgradeEff[i] = cost / (Game.cookiesPs * Game.milkProgress * .050);
-						break;
 					//garden
 					case 470:
-						d_cps = Game.globalCpsMult * Game.ObjectsById[1].storedTotalCps * .02;
+						d_cps = Game.ObjectsById[1].storedTotalCps * Game.globalCpsMult * .02;
 						upgradeEff[i] = cost / ((Game.cookiesPs + d_cps) * .02);
 						break;
 					/*
@@ -116,9 +181,9 @@ Game.registerMod("cookie taster",{
 					474
 					475
 					*/
-					//egg
 					case 65: case 155: case 156: case 476:
-					case 210: case 211: case 212: case 213: case 214: case 215: case 216: case 217: case 218: case 219: case 220: case 221:
+					case 210: case 211: case 212: case 213: case 214: case 215:
+					case 216: case 217: case 218: case 219: case 220: case 221:
 						upgradeEff[i] = cost / (Game.cookiesPs * .01);
 						break;
 					case 66: case 471:
@@ -142,6 +207,7 @@ Game.registerMod("cookie taster",{
 					case 153: case 154:
 						upgradeEff[i] = cost / (Game.cookiesPs * .15);
 						break;
+					//egg
 					/*
 					222:
 					223:
@@ -224,17 +290,16 @@ Game.registerMod("cookie taster",{
 						upgradeEff[i] = cost / (Game.cookiesPs * Game.prestige * heavenlyMult / 100);
 						break;
 					case 130:
-						var base = Game.cookiesPs / (1 + Game.prestige * Game.GetHeavenlyMultiplier() / 100);
-						upgradeEff[i] = cost / ((Game.cookiesPs - base) * denom);
+						upgradeEff[i] = cost / ((Game.cookiesPs - cookiesPsEarthly) * 4);
 						break;
 					case 131:
-						denom -= 1;
+						upgradeEff[i] = cost / (Game.cookiesPs - cookiesPsEarthly);
+						break;
 					case 132:
-						denom -= 1;
+						upgradeEff[i] = cost / ((Game.cookiesPs - cookiesPsEarthly) / 2);
+						break;
 					case 133:
-						denom -= 1;
-						var base = Game.cookiesPs / (1 + Game.prestige * Game.GetHeavenlyMultiplier() / 100);
-						upgradeEff[i] = cost / ((Game.cookiesPs - base) / denom);
+						upgradeEff[i] = cost / ((Game.cookiesPs - cookiesPsEarthly) / 3);
 						break;
 					default:
 						if (Game.UpgradesInStore[i].pool == "cookie") {
